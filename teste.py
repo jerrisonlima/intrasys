5c2b7141743a6b106fa2450607fc8ca4162ecfb8
jerrisonlima <jerrison@gmail.com>
2020-05-19 22:14:06 -0300

    jerrys first commit

---
+++ teste.py
@@ -0,0 +1,16 @@
+x = int(input("Entre com um numero: "))
+    
+if x < 0:
+    print('Numero menor que zero')
+    
+elif x == 0:
+    print('Numero igual a zero')
+      
+elif x == 1:
+    print('Numero igual a 1')
+     
+elif x%2 == 1:
+    print ('Numero Impar')
+
+else:
+    print('Numero Par')
